5e8fba15e75d37defe8b9d411719c693f4b0a45d
Alexandre D'Ambra <Alexandre D'Ambra>
2017-10-26 12:58:12 +0200

    création d'une loop basique, ajout de fichiers supplémentaire pour plus tard et déplacement du code

---
+++ js/monscript.js
@@ -0,0 +1,61 @@
+var camera, scene, renderer;
+var geometry, material, mesh;
+var geometry2, material2, sphere;
+
+var flag = 0;
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+
+  camera.position.z = 1000;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry(200, 200, 200);
+  material = new THREE.MeshBasicMaterial({
+    color: 0xEFD000,
+    wireframe: false
+  });
+
+  mesh = new THREE.Mesh(geometry, material);
+  
+  scene.add(mesh);
+
+  var geometry2 = new THREE.SphereGeometry( 1000, 32, 32 );
+  var material2 = new THREE.MeshBasicMaterial( {color: 0x017EFD, wireframe: true} );
+  sphere = new THREE.Mesh( geometry2, material2 );
+  
+  scene.add( sphere );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+  renderer.render(scene, camera);
+
+}
+
+function animat() {
+
+  requestAnimationFrame(()=>{});
+
+  sphere.rotation.y += 0.001;
+
+  mesh.rotation.z += 0.005;
+  mesh.rotation.y += 0.003;
+  mesh.rotation.x += 0.001;
+
+ /* console.log (sphere);
+  console.log (renderer);
+*/
+  renderer.render(scene, camera);
+
+
+}
+
+setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+
+function onTimerTick() {
+    animat();
+}
---
+++ js/state.js
@@ -0,0 +1,24 @@
+var Game = { };
+
+Game.draw = init();
+Game.update = animat();
+/*
+while (!Game.stopped) { // While the game is running
+  Game.update();        // Update Entities (e.g. Position)
+  Game.draw();          // Draw Entities to the Screen
+}*/
+
+Game.fps = 50;
+
+Game.run = function() {
+  Game.update();
+  Game.draw();
+};
+
+// Start the game loop
+Game._intervalId = setInterval(Game.run, 1000 / Game.fps);
+
+...
+
+// To stop the game, use the following:
+clearInterval(Game._intervalId);
